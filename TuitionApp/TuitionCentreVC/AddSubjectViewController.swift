5c9cbcbd3308dd12cd802fd97f74d2a0af41f685
LihHeng <lihheng90@hotmail.com>
2018-03-06 19:40:49 +0800

    Update Student Subject taken(hardcoded)

--- TuitionApp/TuitionCentreVC/AddSubjectViewController.swift
+++ TuitionApp/TuitionCentreVC/AddSubjectViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
 class AddSubjectViewController: UIViewController {
 
     @IBOutlet weak var studentNameLabel: UILabel!
     @IBOutlet weak var subjectPickerView: UIPickerView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var subjectSelectedLabel: UILabel!
+    @IBOutlet weak var addSubjectButton: UIButton!
     
     let subject = ["Mathematics","Chemistry","XCode","Physics","Moral"]
+    var index : Int = 0
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         subjectPickerView.dataSource = self
         subjectPickerView.delegate = self
+        ref = Database.database().reference()
+
     }
 
     @IBAction func addSubjectButtonTapped(_ sender: Any) {
         
+        let selectedIndex = subjectPickerView.selectedRow(inComponent: 0)
+        let selectedSubject = subject[selectedIndex]
+        print(selectedSubject)
+        let userPost: [String:Any] = [selectedSubject: true]
+        self.ref.child("Tuition").child("Student").child("StudentID").child("Subjects").updateChildValues(userPost)
     }
     
 }
@@ -43,7 +57,8 @@ extension AddSubjectViewController: UIPickerViewDataSource, UIPickerViewDelegate
     }
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        subjectSelected.text = subject[row]
+        subjectSelectedLabel.text = subject[row]
+        index = row
     }
 
 }
@@ -55,7 +70,7 @@ extension AddSubjectViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel
+//        cell.textLabel =
         return cell
     }
     
