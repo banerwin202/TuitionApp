0b344a0a2cbfc7df04bbf8389e5ada87790fcb73
LihHeng <lihheng90@hotmail.com>
2018-03-06 16:30:58 +0800

    Create Student and AddSubject

---
+++ TuitionApp/AddSubjectViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddSubjectViewController.swift
+//  TuitionApp
+//
+//  Created by Lih Heng Yew on 06/03/2018.
+//  Copyright © 2018 Ban Er Win. All rights reserved.
+//
+
+import UIKit
+
+class AddSubjectViewController: UIViewController {
+
+    @IBOutlet weak var studentNameLabel: UILabel!
+    @IBOutlet weak var subjectPickerView: UIPickerView!
+    @IBOutlet weak var addSubjectButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
