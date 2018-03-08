85d42091e54cf87a8e2c4cd8fdb6a9b8471b9c69
cybotchua <cybotchua@hotmail.com>
2018-03-08 16:29:43 +0800

    calendar for uploadscheduleVC set up

--- TuitionApp/ParentsVC/StudentViewController.swift
+++ TuitionApp/ParentsVC/StudentViewController.swift
@@ -456,19 +456,3 @@ extension StudentViewController {
     
 }
 
-extension UIView {
-    func bounce() {
-        self.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.beginFromCurrentState, animations: {
-            self.transform = CGAffineTransform(scaleX: 1, y: 1)
-        }, completion: nil)
-    }
-}
-
-extension Date {
-    func isSmaller(to: Date) -> Bool {
-        return Calendar.current.compare(self, to: to, toGranularity: .day) == .orderedAscending ? true : false
-    }
-}
-
